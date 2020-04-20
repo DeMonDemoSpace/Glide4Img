0863abc4661144eff2d33acf6796d686951872ed
DeMon <lh1995623>
2020-04-20 12:16:45 +0800

    fix

---
+++ app/src/main/java/com/demon/glide4img/ConstUrl.kt
@@ -0,0 +1,19 @@
+package com.demon.glide4img
+
+/**
+ * @author DeMon
+ * Created on 2020/4/17.
+ * E-mail 757454343@qq.com
+ * Desc:
+ */
+object ConstUrl {
+    /**
+     * 自己云服务器上的图片，免费一个月，可能会过期，请替换成自己的图片
+     */
+    const val ImgOne = "http://q8xcfpnt5.bkt.clouddn.com/hzw.jpg"
+
+    const val ImgLogo = "http://q8xcfpnt5.bkt.clouddn.com/hzwlogo.jpg"
+
+    const val ImgGif = "http://q8xcfpnt5.bkt.clouddn.com/hzwgif.gif"
+
+}
--- app/src/main/java/com/demon/glide4img/UseBaseActivity.kt
+++ app/src/main/java/com/demon/glide4img/UseBaseActivity.kt
@@ -1,5 +1,6 @@
 package com.demon.glide4img
 
+import android.app.ProgressDialog
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,9 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.*
 import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.activity_use_base.*
 import java.io.File
 
@@ -19,32 +22,64 @@ class UseBaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_use_base)
-        val url = "http://guolin.tech/book.png"
+
+        Glide.with(this).load(ConstUrl.ImgLogo).into(ivLogo)
+
         val options = RequestOptions()
             .placeholder(R.mipmap.loading)
             .error(R.mipmap.error)
             .skipMemoryCache(true)
             //.override(100, 200)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
-        Glide.with(this).load(url).apply(options).into(ivUrl)
+        Glide.with(this).load(ConstUrl.ImgOne).apply(options).into(ivUrl)
+
+
+        val dialog = ProgressDialog(this)
+        val simpleTarget = object : DrawableImageViewTarget(ivInto) {
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                super.onResourceReady(resource, transition)
+                dialog.dismiss()
+            }
+
+            override fun onLoadStarted(placeholder: Drawable?) {
+                super.onLoadStarted(placeholder)
+                dialog.show()
+            }
+
+            override fun onLoadFailed(errorDrawable: Drawable?) {
+                super.onLoadFailed(errorDrawable)
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                super.onLoadCleared(placeholder)
+            }
+
+            override fun onStart() {
+                super.onStart()
+            }
+
+            override fun onStop() {
+                super.onStop()
+            }
+
+            override fun onDestroy() {
+                super.onDestroy()
+            }
+        }
+
+        val optionInto = RequestOptions()
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+        Glide.with(this).load(ConstUrl.ImgOne).apply(optionInto).into(simpleTarget)
 
 
         Glide.with(this)
             .asBitmap()
-            .load("http://guolin.tech/test.gif")
+            .load(ConstUrl.ImgGif)
             .into(ivGiftFirst)
 
         Glide.with(this)
-            .load("http://guolin.tech/test.gif")
-            .listener(object :RequestListener<Drawable>{
-                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                    TODO("Not yet implemented")
-                }
-            })
+            .load(ConstUrl.ImgGif)
             .into(ivGift)
     }
 }
